40e0da3538406b59756e5dc949ed034ccf06ed3f
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-08-29 05:13:50 +0300

    Update main.c

--- C/Y2023-Semester1/Ville-tasks/Ex-14/main.c
+++ C/Y2023-Semester1/Ville-tasks/Ex-14/main.c
@@ -20,12 +20,15 @@ const char *lang2str(language lang);
 
 int main()
 {
-    
+    const char* monthName = month(1, English);
+    printf("Month: %s\n", monthName);
+
     return 0;
 }
 
 const char *month(int number, language lang)
 {
+    printf("month: %d, lang: %d\n", number, lang);
     const char *months[3][12] = {
         {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
          "November", "December"},
@@ -34,18 +37,30 @@ const char *month(int number, language lang)
         {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
          "Octubre", "Noviembre", "Diciembre"}
     };
+    const char *error = "Error";
 
     if (number < 1 || number > 12)
     {
-        return "Error";
+        return error;
     }
+
+    //printf("%s", months[lang][number - 1]);
 
     return months[lang][number - 1];
 }
 
 const char *lang2str(language lang)
 {
+    //printf("lang: %d\n", lang);
     const char *languages[3] = {"English", "Italian", "Spanish"};
+    const char *error = "Error";
 
-    return languages[lang];
+    if (lang < 0 || lang > 3)
+    {
+        return error;
+    }
+    else
+    {
+        return languages[lang];
+    }
 }
