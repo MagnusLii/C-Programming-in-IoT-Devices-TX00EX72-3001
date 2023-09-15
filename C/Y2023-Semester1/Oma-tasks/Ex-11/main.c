1283a72343a140c94743309b59df4998a9fd6494
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-09-15 15:01:22 +0300

    Finished Ex10-12

--- C/Y2023-Semester1/Oma-tasks/Ex-11/main.c
+++ C/Y2023-Semester1/Oma-tasks/Ex-11/main.c
@@ -8,22 +8,31 @@ void fgets_thingy(char *str, int size);
 
 int main(void)
 {
-    //char text[80] = "I am so tired of Python. C is much better language";
+    // char text[80] = "I am so tired of Python. C is much better language";
+    // char replace[3] = "e3";
     int count = 0;
-    char text[80], replace[3];
+    char text[80] = "\0", replace[3] = "\0";
     size_t size;
-    
+
     printf("Enter a string: ");
     fgets_thingy(text, 80);
 
     printf("Enter a string containing two characters: ");
     fgets_thingy(replace, 3);
 
-    //count = replace_char(text, "e3");
+    count = replace_char(text, replace);
 
     printf("\n\n");
-    printf("The number of characters replaced is: %d\n", count);
-    printf("The modified string is: %s\n", text);
+
+    if (count == 0)
+    {
+        printf("String was not modified\n");
+    }
+    else
+    {
+        printf("The number of characters replaced is: %d\n", count);
+        printf("The modified string is: %s\n", text);
+    }
     return 0;
 }
 
@@ -47,11 +56,9 @@ int replace_char(char *str, const char *repl)
 void fgets_thingy(char *str, int size)
 {
     fgets(str, size, stdin);
-    printf("%s", str);
     size_t input_length = strlen(str);
     if (input_length > 0 && str[input_length - 1] == '\n')
     {
         str[input_length - 1] = '\0';
     }
-    
 }
--- C/Y2023-Semester1/Oma-tasks/Ex-12/main.c
+++ C/Y2023-Semester1/Oma-tasks/Ex-12/main.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int count_words(const char *str, const char *word);
+void fgets_thingy(char *str, int size);
 
 int main(void)
 {
-    
+    // char str[100] = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse";
+    // char word[100] = "ip";
+
+    char str[100] = "\0";
+    char word[100] = "\0";
+    int changes = 0;
+
+    while (strcmp(word, "stop") != 0)
+    {
+        printf("Enter a string: ");
+        fgets_thingy(str, 100);
+        printf("Enter a word\nOr enter \"stop\" to stop the program: ");
+        fgets_thingy(word, 100);
+
+        changes = count_words(str, word);
+        if (strcmop(word, "stop") != 0)
+        {
+            printf("\nThe word \"%s\" occurs %d times in the string \"%s\"\n\n", word, changes, str);
+        }
+        
+    }
     return 0;
+}
+
+int count_words(const char *str, const char *word)
+{
+    int count = 0;
+    char *ptr = strstr(str, word);
+    while (ptr != NULL)
+    {
+        count++;
+        ptr = strstr(ptr + 1, word);
+    }
+    return count;
+}
+
+void fgets_thingy(char *str, int size)
+{
+    fgets(str, size, stdin);
+    size_t input_length = strlen(str);
+    if (input_length > 0 && str[input_length - 1] == '\n')
+    {
+        str[input_length - 1] = '\0';
+    }
 }
