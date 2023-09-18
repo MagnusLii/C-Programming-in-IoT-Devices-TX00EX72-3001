c892eb89e3508e38feaa7fc8672eefae71773ea6
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-09-18 10:55:25 +0300

    add ex13

---
+++ C/Y2023-Semester1/Oma-tasks/Ex-13/main.c
@@ -0,0 +1,15 @@
+/*Write a program that asks user to enter a filename. Then program tries to open the file in textmode
+for reading. If opening the file fails the program prints an error message with the filename to stderr
+and then exits. If the file is opened the program starts reading integers from the file until reading
+fails or the file ends. When reading stops the program prints the count of numbers and the lowest
+and highest number that was read from the file. Then program closes the file.
+For testing make a few text files with integers using your favourite text editor.*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    
+    return 0;
+}
