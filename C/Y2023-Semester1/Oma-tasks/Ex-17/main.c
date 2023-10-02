f0494f2853241893b865088ba7a25cc665caa539
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-10-02 09:58:34 +0300

    Fixed main loop logic for Ex-17

--- C/Y2023-Semester1/Oma-tasks/Ex-17/main.c
+++ C/Y2023-Semester1/Oma-tasks/Ex-17/main.c
@@ -4,11 +4,13 @@
 #include <stdbool.h>
 #include <time.h>
 
-#define INPUT_BUFFER_LENGHT 256
+#define INPUT_BUFFER_LENGHT 32
+#define MAX_PASSWORD_LENGHT INPUT_BUFFER_LENGHT * 2 + 1
 
 bool generatePassword(char *password, int size, const char *word);
 bool improvedFgets(char *stringToStoreTo, const int maxLenghtOfString);
-
+bool exitToCancel(const char *inputStr);
+void convertToLowercase(char *str);
 
 int main()
 {
@@ -16,13 +18,25 @@ int main()
 
     char input[INPUT_BUFFER_LENGHT];
     char password[(INPUT_BUFFER_LENGHT * 2) + 1];
+    bool stop = false, inputLen = false;
 
-    printf("Enter base to generate password from: ");
-    improvedFgets(input, INPUT_BUFFER_LENGHT);
-
-    if (generatePassword(password, sizeof(password), input) == true)
+    while (stop == false)
     {
-        printf("Generated password: %s\n", password);
+        printf("Enter base to generate password from: ");
+        inputLen = improvedFgets(input, INPUT_BUFFER_LENGHT);
+
+        if (exitToCancel(input) == true)
+        {
+            stop = true;
+        }
+        else if (generatePassword(password, MAX_PASSWORD_LENGHT, input) == true && inputLen == true)
+        {
+            printf("Generated password: %s\n", password);
+        }
+        else
+        {
+            printf("Error: incorrect input.\n");
+        }
     }
 
     return 0;
@@ -32,6 +46,7 @@ bool improvedFgets(char *stringToStoreTo, const int maxLenghtOfString)
 {
     bool newline_found = false; // Flag to track the presence of '\n'.
     int i = 0;
+    char dump[1000] = "\0";
     if (fgets(stringToStoreTo, INPUT_BUFFER_LENGHT, stdin) != NULL)
     {
         // Checking for empty input.
@@ -53,6 +68,7 @@ bool improvedFgets(char *stringToStoreTo, const int maxLenghtOfString)
             if (i >= maxLenghtOfString - 1)
             {
                 printf("\nError: Input over max accepted lenght of %d characters.\n", maxLenghtOfString - 1);
+                fgets(dump, 1000, stdin);
                 return false;
             }
             i++;
@@ -61,9 +77,9 @@ bool improvedFgets(char *stringToStoreTo, const int maxLenghtOfString)
     return true;
 }
 
-bool generatePassword(char *password, int size, const char *input)
+bool generatePassword(char *password, const int size, const char *input)
 {
-    if (size < ((strlen(input) * 2) + 3))
+    if (size < ((strlen(input) * 2) + 1))
     {
         printf("Error: your input is too long.\n");
         return false;
@@ -86,4 +102,29 @@ bool generatePassword(char *password, int size, const char *input)
     password[passwordLen] = '\0';
 
     return true;
+}
+
+bool exitToCancel(const char *inputStr)
+{
+    // Convert the input string to lowercase for case-insensitive comparison.
+    char inputStrLower[INPUT_BUFFER_LENGHT] = "\0";
+    int i = 0;
+
+    strcpy(inputStrLower, inputStr);
+    convertToLowercase(inputStrLower);
+
+    if (strcmp(inputStrLower, "stop") == 0)
+    {
+        printf("Cancelling...\n");
+        return true; // Return true to indicate the "exit" command was detected.
+    }
+    return false; // Return false if the input is not "exit".
+}
+
+void convertToLowercase(char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        str[i] = tolower(str[i]);
+    }
 }
