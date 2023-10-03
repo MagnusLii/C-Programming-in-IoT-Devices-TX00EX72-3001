1394a66aa04a713e1431d669b6f3d2a03fe5c05d
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-10-03 04:51:30 +0300

    Ex-20 initial commit

--- C/Y2023-Semester1/Oma-tasks/Ex-20/main.c
+++ C/Y2023-Semester1/Oma-tasks/Ex-20/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "debug.h"
 
-int main(void)
-{
-    
+int main() {
+    srand(time(NULL));
+
+    int debug_level;
+    printf("Enter debug level (0-4): ");
+    scanf("%d", &debug_level);
+
+    if (debug_level < 0 || debug_level > 4) {
+        printf("Invalid debug level. Please enter a number between 0 and 4.\n");
+        return 1;
+    }
+
+    set_debug_level(debug_level);
+
+    for (int i = 1; i <= 5; i++) {
+        int random_debug_level = rand() % 5; // Generate random debug level between 0 and 4
+        dprintf(random_debug_level, "This is message %d\n", i);
+    }
+
     return 0;
 }
