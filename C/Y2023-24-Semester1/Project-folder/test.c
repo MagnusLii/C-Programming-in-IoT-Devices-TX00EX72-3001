973d3bbb5a1cf4de7ab352a8be835a182fe7d153
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-10-13 20:25:54 +0300

    Update test.c

--- C/Y2023-24-Semester1/Project-folder/test.c
+++ C/Y2023-24-Semester1/Project-folder/test.c
@@ -4,33 +4,51 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <ctype.h>
+#include <stdlib.h>
 
-bool validateFormat(const char *input);
+
+bool fileRead(char *stringToStoreTo, FILE *fileToReadFrom);
 
 int main()
 {
 
     FILE *pFile = fopen("db.txt", "r");
-
     char string[256] = {0};
-    int readCount = fscanf(pFile, "%s", string);
 
-    validateFormat(string);
-
+    while (fileRead(string ,pFile) != false)
+    {
+        printf("%s", string);
+    }
+    
     return 0;
 }
 
-bool validateFormat(const char *input)
-{
-    int val1, val2;
-    if (sscanf(input, "%d %d", &val1, &val2) == 2)
-    {
-        printf("The input matches the expected format.\n");
-        return true;
-    }
-    else
-    {
-        printf("The input does not match the expected format.\n");
+bool fileRead(char *stringToStoreTo, FILE *fileToReadFrom){
+    char *buffer;
+    size_t line_len = 0;
+    ssize_t bufferLen;
+    buffer = (char *)malloc(line_len * sizeof(char));
+    if( buffer == NULL){
+        fprintf(stdout, "Error: Unable to allocate buffer in fileRead.\n");
+        free(buffer);
         return false;
     }
+
+    bufferLen = getline(&buffer, &line_len, fileToReadFrom);
+    if (bufferLen == -1 || bufferLen == 0){
+        fprintf(stdout, "Error: Failed to read data from the file '%s'\n", "db.txt");
+        free(buffer);
+        return false;
+    }
+    else if (bufferLen > 256){
+        fprintf(stdout, "Error: Line length exceeds buffer length in fileRead.\n");
+        free(buffer);
+        return false;
+    }
+
+    strncpy(stringToStoreTo, buffer, sizeof(stringToStoreTo) - 1);
+    stringToStoreTo[sizeof(stringToStoreTo) - 1] = '\0';
+    free(buffer);
+
+    return true;
 }
