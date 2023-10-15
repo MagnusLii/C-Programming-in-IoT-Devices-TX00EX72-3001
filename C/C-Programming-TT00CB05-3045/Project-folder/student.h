4740a0149af1b0f34bb426d559fc8d6a09e4e70d
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-10-15 23:42:16 +0300

    Fixed typo

--- C/C-Programming-TT00CB05-3045/Project-folder/student.h
+++ C/C-Programming-TT00CB05-3045/Project-folder/student.h
@@ -3,15 +3,15 @@
 
 #include <stdio.h>
 
-#define STUDEN_ARR_LEN 256
+#define STUDENT_ARR_LEN 256
 
 struct Student
 {
     int studentind, db_entry_row;
-    char firstname[STUDEN_ARR_LEN];
-    char lastname[STUDEN_ARR_LEN];
-    char studentid[STUDEN_ARR_LEN];
-    char major[STUDEN_ARR_LEN];
+    char firstname[STUDENT_ARR_LEN];
+    char lastname[STUDENT_ARR_LEN];
+    char studentid[STUDENT_ARR_LEN];
+    char major[STUDENT_ARR_LEN];
     int fetchFailure;
 };
 
