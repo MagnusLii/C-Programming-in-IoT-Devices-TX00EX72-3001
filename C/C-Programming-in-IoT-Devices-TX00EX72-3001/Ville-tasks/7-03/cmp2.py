06161e8d09951e34e5df1d8f8da674dacaf7f553
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-10-22 18:01:36 +0300

    add cmp

---
+++ C/C-Programming-in-IoT-Devices-TX00EX72-3001/Ville-tasks/7-03/cmp2.py
@@ -0,0 +1,66 @@
+"""
+[37]UPON a time, before the faery broods
+[49]Drove Nymph and Satyr from the prosperous woods,
+[36]Before King Oberon's bright diadem,
+[44]Sceptre, and mantle, clasp'd with dewy gem,
+[39]Frighted away the Dryads and the Fauns
+[52]From rushes green, and brakes, and cowslip'd lawns,
+[35]The ever-smitten Hermes empty left
+[47]His golden throne, bent warm on amorous theft:
+[39]From high Olympus had he stolen light,
+[51]On this side of Jove's clouds, to escape the sight
+[40]Of his great summoner, and made retreat
+[38]Into a forest on the shores of Crete.
+[42]For somewhere in that sacred island dwelt
+[42]A nymph, to whom all hoofed Satyrs knelt;
+[47]At whose white feet the languid Tritons poured
+[48]Pearls, while on land they wither'd and adored.
+[49]Fast by the springs where she to bathe was wont,
+[51]And in those meads where sometime she might haunt,
+[45]Were strewn rich gifts, unknown to any Muse,
+[47]Though Fancy's casket were unlock'd to choose.
+[42]Ah, what a world of love was at her feet!
+[40]So Hermes thought, and a celestial heat
+[43]Burnt from his winged heels to either ear,
+[42]That from a whiteness, as the lily clear,
+[41]Blush'd into roses 'mid his golden hair,
+[50]Fallen in jealous curls about his shoulders bare.
+[47]From vale to vale, from wood to wood, he flew,
+[44]Breathing upon the flowers his passion new,
+[41]And wound with many a river to its head,
+[56]To find where this sweet nymph prepar'd her secret bed:
+[49]In vain; the sweet nymph might nowhere be found,
+[40]And so he rested, on the lonely ground,
+[40]Pensive, and full of painful jealousies
+[43]Of the Wood-Gods, and even the very trees.
+[46]There as he stood, he heard a mournful voice,
+[46]Such as once heard, in gentle heart, destroys
+[46]All pain but pity: thus the lone voice spake:
+[45]"When from this wreathed tomb shall I awake!
+[41]"When move in a sweet body fit for life,
+[46]"And love, and pleasure, and the ruddy strife
+[40]"Of hearts and lips! Ah, miserable me!"
+[38]The God, dove-footed, glided silently
+[50]Round bush and tree, soft-brushing, in his speed,
+[44]The taller grasses and full-flowering weed,
+[36]Until he found a palpitating snake,
+[46]Bright, and cirque-couchant in a dusky brake.
+[79]Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lore
+[79]m Ipsum has been the industry's standard dummy text ever since the 1500s, when 
+[79]an unknown printer took a galley of type and scrambled it to make a type specim
+[79]en book. It has survived not only five centuries, but also the leap into electr
+[79]onic typesetting, remaining essentially unchanged. It was popularised in the 19
+[79]60s with the release of Letraset sheets containing Lorem Ipsum passages, and mo
+[79]re recently with desktop publishing software like Aldus PageMaker including ver
+[21]sions of Lorem Ipsum.
+[79]Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
+[79] in a piece of classical Latin literature from 45 BC, making it over 2000 years
+[79] old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virgin
+[79]ia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ip
+[79]sum passage, and going through the cites of the word in classical literature, d
+[79]iscovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1
+[79].10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Ci
+[79]cero, written in 45 BC. This book is a treatise on the theory of ethics, very p
+[79]opular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolo
+[52]r sit amet..", comes from a line in section 1.10.32.
+"""
