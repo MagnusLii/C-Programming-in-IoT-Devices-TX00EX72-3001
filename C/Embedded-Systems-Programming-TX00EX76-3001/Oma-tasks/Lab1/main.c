4d83194daaafa6de9863350f7f34013b02ff0fe3
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-10-26 23:24:02 +0300

    update to main and added relevant pdfs

--- C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab1/main.c
+++ C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab1/main.c
@@ -1,32 +1,32 @@
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 
-#define BUTTON_ON_OFF 7
-#define BUTTON_INC 8
-#define BUTTON_DEC 9
+#define BUTTON_ON_OFF 8
+#define BUTTON_INC 9
+#define BUTTON_DEC 7
 
 #define N_LED 3
 #define STARTING_LED 20
-#define LED1 20
-#define LED2 21
-#define LED3 22
 
-void inc_dutycycle(int *dutycycle){
-    if (*dutycycle < 99){
-        *dutycycle = *dutycycle + 10;
-        if (*dutycycle >= 99)
+int dutycycle = 75;
+bool led_state = false;
+
+void inc_dutycycle(){
+    if (dutycycle < 99){
+        dutycycle = dutycycle + 10;
+        if (dutycycle >= 99)
         {
-            *dutycycle = 99;
+            dutycycle = 99;
         }
     }
 }
 
-void dec_dutycycle(int *dutycycle){
-    if (*dutycycle > 1){
-        *dutycycle = *dutycycle - 10;
-        if (*dutycycle <= 1)
+void dec_dutycycle(){
+    if (dutycycle > 1){
+        dutycycle = dutycycle - 10;
+        if (dutycycle <= 1)
         {
-            *dutycycle = 1;
+            dutycycle = 1;
         }
     }
 }
@@ -44,9 +44,22 @@ uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, int d){
     return wrap;
 }
 
-int main(){
-    int dutycycle = 75;
+void toggle_leds(){
+    led_state = !led_state;
+    for (int i = STARTING_LED; i < STARTING_LED + N_LED; i++){
+        gpio_set_function(i, GPIO_FUNC_PWM);
+        uint slice_num = pwm_gpio_to_slice_num(i);
+        uint chan = pwm_gpio_to_channel(i);
+        if(led_state) {
+            pwm_set_freq_duty(slice_num, chan, 50, dutycycle);
+            pwm_set_enabled(slice_num, true);
+        } else {
+            pwm_set_enabled(slice_num, false);
+        }
+    }
+}
 
+int main(){
     // setup led(s).
     for (int i = STARTING_LED; i < STARTING_LED + N_LED; i++){
         gpio_set_function(i, GPIO_FUNC_PWM);
@@ -56,40 +69,57 @@ int main(){
         pwm_set_enabled(slice_num, true);
     }
 
-    // setup button pin 8.
+    // setup button pin for on/off.
+    gpio_init(BUTTON_ON_OFF);
+    gpio_set_dir(BUTTON_ON_OFF, GPIO_IN);
+    gpio_pull_up(BUTTON_ON_OFF);
+
+    // setup button pin for increase.
     gpio_init(BUTTON_INC);
     gpio_set_dir(BUTTON_INC, GPIO_IN);
     gpio_pull_up(BUTTON_INC);
 
-    // setup button pin 9.
+    // setup button pin for decrease.
     gpio_init(BUTTON_DEC);
     gpio_set_dir(BUTTON_DEC, GPIO_IN);
     gpio_pull_up(BUTTON_DEC);
 
+    bool prev_on_off_state = false;
+
     while (1){
-        if (gpio_get(BUTTON_INC) == 0){
-            inc_dutycycle(&dutycycle);
-            for (int i = STARTING_LED; i < STARTING_LED + N_LED; i++){
-                gpio_set_function(i, GPIO_FUNC_PWM);
-                uint slice_num = pwm_gpio_to_slice_num(i);
-                uint chan = pwm_gpio_to_channel(i);
-                pwm_set_freq_duty(slice_num, chan, 50, dutycycle);
-                pwm_set_enabled(slice_num, true);
-            }
-            
-            sleep_ms(100);
+        bool on_off_state = gpio_get(BUTTON_ON_OFF) == 0;
+        if (on_off_state && !prev_on_off_state){
+            toggle_leds();
+            sleep_ms(100); // Debounce delay
         }
-        if (gpio_get(BUTTON_DEC) == 0){
-            dec_dutycycle(&dutycycle);
-            for (int i = STARTING_LED; i < STARTING_LED + N_LED; i++){
-                gpio_set_function(i, GPIO_FUNC_PWM);
-                uint slice_num = pwm_gpio_to_slice_num(i);
-                uint chan = pwm_gpio_to_channel(i);
-                pwm_set_freq_duty(slice_num, chan, 50, dutycycle);
-                pwm_set_enabled(slice_num, true);
+        prev_on_off_state = on_off_state;
+
+        if(led_state) {
+            if (gpio_get(BUTTON_INC) == 0){
+                inc_dutycycle();
+                for (int i = STARTING_LED; i < STARTING_LED + N_LED; i++){
+                    gpio_set_function(i, GPIO_FUNC_PWM);
+                    uint slice_num = pwm_gpio_to_slice_num(i);
+                    uint chan = pwm_gpio_to_channel(i);
+                    pwm_set_freq_duty(slice_num, chan, 50, dutycycle);
+                    pwm_set_enabled(slice_num, true);
+                }
+                
+                sleep_ms(100); // Smooth dimming delay
             }
-            sleep_ms(100);
+            if (gpio_get(BUTTON_DEC) == 0){
+                dec_dutycycle();
+                for (int i = STARTING_LED; i < STARTING_LED + N_LED; i++){
+                    gpio_set_function(i, GPIO_FUNC_PWM);
+                    uint slice_num = pwm_gpio_to_slice_num(i);
+                    uint chan = pwm_gpio_to_channel(i);
+                    pwm_set_freq_duty(slice_num, chan, 50, dutycycle);
+                    pwm_set_enabled(slice_num, true);
+                }
+                sleep_ms(100); // Smooth dimming delay
+            }
         }
+        
     }
-    return 0;
+    
 }
