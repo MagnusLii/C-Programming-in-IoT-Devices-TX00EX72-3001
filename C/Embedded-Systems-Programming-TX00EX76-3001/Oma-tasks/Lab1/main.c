a3826dcd3f383665ffa780e039ecece7dbee6793
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-10-26 22:30:04 +0300

    Update main.c

--- C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab1/main.c
+++ C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab1/main.c
@@ -1,74 +1,27 @@
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 
-#define BUTTON_ON_OFF 7
-#define BUTTON_INC 8
-#define BUTTON_DEC 9
+uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, int d)
+{
+    uint32_t clock = 125000000;
+    uint32_t divider16 = clock / f / 4096 + (clock % (f * 4096) != 0);
+    if (divider16 / 16 == 0)
+        divider16 = 16;
 
-#define LED1 20
-#define LED2 21
-#define LED3 22
+    uint32_t wrap = clock * 16 / divider16 / f - 1;
+    pwm_set_clkdiv_int_frac(slice_num, divider16 / 16, divider16 & 0xF);
+    pwm_set_wrap(slice_num, wrap);
+    pwm_set_chan_level(slice_num, chan, wrap * d / 100);
+    return wrap;
+}
 
 int main()
 {
-    
-    gpio_set_function(LED1, GPIO_FUNC_PWM);
-    gpio_set_function(LED2, GPIO_FUNC_PWM);
-    gpio_set_function(LED3, GPIO_FUNC_PWM);
-
-    uint slice_num1 = pwm_gpio_to_slice_num(LED1);
-    uint slice_num2 = pwm_gpio_to_slice_num(LED2);
-    uint slice_num3 = pwm_gpio_to_slice_num(LED3);
-
-    pwm_set_enabled(slice_num1, true);
-    pwm_set_enabled(slice_num2, true);
-    pwm_set_enabled(slice_num3, true);
-
-    pwm_set_wrap(slice_num1, 255);
-    pwm_set_wrap(slice_num2, 255);
-    pwm_set_wrap(slice_num3, 255);
-
-    pwm_set_chan_level(slice_num1, PWM_CHAN_A, 0);
-    pwm_set_chan_level(slice_num2, PWM_CHAN_A, 0);
-    pwm_set_chan_level(slice_num3, PWM_CHAN_A, 0);
-
-    // Initialize buttons
-    gpio_init(BUTTON_ON_OFF);
-    gpio_init(BUTTON_INC);
-    gpio_init(BUTTON_DEC);
-
-    gpio_set_dir(BUTTON_ON_OFF, GPIO_IN);
-    gpio_set_dir(BUTTON_INC, GPIO_IN);
-    gpio_set_dir(BUTTON_DEC, GPIO_IN);
-
-    // Initialize LEDs
-    gpio_init(LED1);
-    gpio_init(LED2);
-    gpio_init(LED3);
-
-    gpio_set_dir(LED1, GPIO_OUT);
-    gpio_set_dir(LED2, GPIO_OUT);
-    gpio_set_dir(LED3, GPIO_OUT);
-
-    while (true) {
-        if (gpio_get(BUTTON_ON_OFF)) {
-            toggle_leds();
-            sleep_ms(200); // Debounce delay
-        }
-
-        if (led_state) {
-            if (gpio_get(BUTTON_INC)) {
-                increase_brightness();
-                sleep_ms(10); // Smooth dimming delay
-            }
-
-            if (gpio_get(BUTTON_DEC)) {
-                decrease_brightness();
-                sleep_ms(10); // Smooth dimming delay
-            }
-        }
-    }
-
+    gpio_set_function(22, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(22);
+    uint chan = pwm_gpio_to_channel(22);
+    pwm_set_freq_duty(slice_num, chan, 50, 75);
+    pwm_set_enabled(slice_num, true);
     return 0;
 }
 
