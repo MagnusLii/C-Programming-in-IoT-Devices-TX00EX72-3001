c9e1f5c61d927fc69dd2483de3c567f9bfb23c02
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-10-30 10:50:15 +0200

    Update main.c

--- C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab1/main.c
+++ C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab1/main.c
@@ -33,7 +33,7 @@ void dec_dutycycle(int *dutycycle){
     }
 }
 
-uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, int d){
+uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, int dutycycle){
     uint32_t clock = 125000000;
     uint32_t divider16 = clock / f / 4096 + (clock % (f * 4096) != 0);
     if (divider16 / 16 == 0){
