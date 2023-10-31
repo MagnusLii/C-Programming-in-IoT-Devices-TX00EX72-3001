380851af97bc9c1b2b4a77d710265b477cd50014
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-10-31 09:19:33 +0200

    Update main.c

--- C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab02/main.c
+++ C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab02/main.c
@@ -72,6 +72,7 @@ void gpio_callback(uint gpio, uint32_t events){
         led_status_changed = true;
         while (button_debounced == false){
             sleep_ms(50);
+            printf("button_checks: %d\n", button_checks);
             if (button_checks > 3)
             {
                 button_debounced = true;
