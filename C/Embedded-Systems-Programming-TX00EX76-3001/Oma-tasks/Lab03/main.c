6f297ee6a057136c3c5ff3e9992cec67e1dc02f4
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-11-08 18:42:21 +0200

    test

--- C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab03/main.c
+++ C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab03/main.c
@@ -39,35 +39,6 @@ bool read_response(int max_attempts) {
     return false;
 }
 
-void read_dev_eui() {
-    send_command("AT+COMMAND=DATA"); // Set value format
-    sleep_ms(TIMEOUT_MS);
-    char response[STRLEN];
-    int pos = 0;
-    while (uart_is_readable(UART_ID)) {
-        char c = uart_getc(UART_ID);
-        if (c == '\r' || c == '\n') {
-            response[pos] = '\0';
-            printf("received: %s\n", response);
-            pos = 0;  // Start over after the line is printed
-            // Extract and format DevEui
-            char* deveui_start = strstr(response, "+COMMAND: ");
-            if (deveui_start != NULL) {
-                deveui_start += 11;  // Move to the beginning of the DevEui value
-                for (size_t i = 0; i < strlen(deveui_start); i++) {
-                    if (deveui_start[i] != ' ') {
-                        putchar(tolower(deveui_start[i]));
-                    }
-                }
-            }
-        } else {
-            if (pos < STRLEN - 1) {
-                response[pos++] = c;
-            }
-        }
-    }
-}
-
 int main() {
     stdio_init_all();
 
@@ -84,7 +55,7 @@ int main() {
     printf("LoRa module test\n");
     sleep_ms(1000);
 
-    while (1) {
+    while (true) {
         if (state == 1) {
             printf("Press SW_0 to start communication with the LoRa module...\n");
             while (gpio_get(SW_0_PIN)) {
@@ -102,11 +73,15 @@ int main() {
                 state = 1;
             }
         } else if (state == 3) {
-            printf("Reading DevEui...\n");
-            read_dev_eui();
-            printf("\n");
-            state = 1;
+            printf("Reading firmware ver...\n");
+            send_command("AT+VER\r\n");
+            if (read_response(5) == true) {
+                printf("\n");
+            } else {
+                printf("Module not responding\n");
             }
+            state = 1;
+        }
     }
     return 0;
 }
