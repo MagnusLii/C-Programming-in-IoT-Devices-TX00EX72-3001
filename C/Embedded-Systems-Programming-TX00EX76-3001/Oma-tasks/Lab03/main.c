8de1dea46ad23c93a2194cd40f04f2b664e379d5
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-11-08 18:15:09 +0200

    test

--- C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab03/main.c
+++ C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab03/main.c
@@ -29,8 +29,8 @@ bool read_response(const char expected_response, const char command, int max_att
     }
 
     for (int max_retries = 0; max_retries < max_attempts; max_retries++){
-        printf("Sending command %s", &command)
-        send_command(&command);
+        printf("Sending command %s", command);
+        send_command(command);
         sleep_ms(TIMEOUT_MS);
         while (uart_is_readable(UART_ID)) {
             char c = uart_getc(UART_ID);
