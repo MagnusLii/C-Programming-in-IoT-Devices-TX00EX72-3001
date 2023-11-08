3de1907165df707b71955d3a44bc61c1e9b15b82
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-11-08 22:47:56 +0200

    test

--- C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab03/test.c
+++ C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab03/test.c
@@ -3,6 +3,7 @@
 #include "hardware/uart.h"
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #define SW_0_PIN 9
 #define TX_PIN 4
@@ -13,10 +14,10 @@
 #define BUFFER_SIZE 256
 
 void send_command(const char* command);
-bool read_response(const char expected_response[], const int response_len, int max_attempts, char *pstring_to_store_to);
+bool read_response(const char expected_response[], int response_len, int max_attempts, char *pstring_to_store_to);
 void uart_rx_handler();
-bool process_uart_data(const char expected_response[], const int response_len, char *pstring_to_store_to);
-void process_DevEui(char DevEui[], const int DevEui_len);
+bool process_uart_data(const char expected_response[], int response_len, char *pstring_to_store_to);
+void process_DevEui(char DevEui[], int DevEui_len);
 
 char circular_buffer[BUFFER_SIZE];
 volatile int buffer_head = 0;
