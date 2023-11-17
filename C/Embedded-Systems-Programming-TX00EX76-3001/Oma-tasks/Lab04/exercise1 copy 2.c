173dd1fd9c0526bba0fe9c87a794924d71b07cc0
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-11-17 18:26:54 +0200

    Create exercise1 copy 2.c

---
+++ C/Embedded-Systems-Programming-TX00EX76-3001/Oma-tasks/Lab04/exercise1 copy 2.c
@@ -0,0 +1,105 @@
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+#include "hardware/i2c.h"
+#include "hardware/gpio.h"
+#include "pico/util/queue.h"
+#include <stdio.h>
+
+#define ROT_A 10
+#define ROT_B 11
+#define ROT_SW 12
+
+#define BUTTON1_PIN 7
+#define BUTTON2_PIN 8
+#define BUTTON3_PIN 9
+#define SDA_PIN 17
+#define SCL_PIN 16
+
+#define N_LED 3
+#define STARTING_LED 20
+#define LED_BRIGHT_MAX 999
+#define LED_BRIGHT_MIN 0
+#define LED_BRIGHT_STEP 10
+
+#define EEPROM_ADDR 0x50  // I2C address of the EEPROM
+#define BRIGHTNESS_ADDR 2
+#define EEPROM_WRITE_DELAY_MS 5
+#define LED_STATE_ADDR 32768 // Address in the EEPROM to store the LED state
+#define BUFFER_SIZE 512
+
+
+typedef struct ledStatus {
+    uint16_t led_state[3];
+    uint16_t brightness;
+} ledStatus;
+
+typedef struct queueTrain {
+    uint16_t gpioPin;
+    uint16_t pinState;
+} queueTrain;
+
+static queue_t irqEvents;
+
+static void gpio_handler(uint gpio, uint32_t event_mask)
+{
+    queue_try_add(&irqEvents, &gpio);
+}
+
+void main(){
+    char OnOff[2][10] = {"OFF", "ON"};
+
+    stdio_init_all();
+
+    // init the I2C bus
+    i2c_init(i2c_default, 100 * 1000); //100khz
+    gpio_set_function(SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(SDA_PIN);
+    gpio_pull_up(SCL_PIN);
+
+    // setup button pin for increase.
+    gpio_init(ROT_A);
+    gpio_set_dir(ROT_A, GPIO_IN);
+
+    // setup button pin for decrease.
+    gpio_init(ROT_B);
+    gpio_set_dir(ROT_B, GPIO_IN);
+
+    // setup buttons
+    for (int i = BUTTON1_PIN; i < BUTTON1_PIN + N_LED; i++){
+        gpio_init(i);
+        gpio_set_dir(i, GPIO_IN);
+        gpio_pull_up(i);
+    }
+
+    // setup led(s).
+    for (int i = STARTING_LED; i < STARTING_LED + N_LED; i++){
+        uint slice_num = pwm_gpio_to_slice_num(i);
+        uint chan = pwm_gpio_to_channel(i);
+        pwm_set_enabled(slice_num, false);
+        pwm_config config = pwm_get_default_config();
+        pwm_config_set_clkdiv_int(&config, 125);
+        pwm_config_set_wrap(&config, 1000); // 1kHz
+        pwm_init(slice_num, &config, false);
+        //pwm_set_chan_level(slice_num, chan, ls.brightness); // brightness
+        gpio_set_function(i, GPIO_FUNC_PWM);
+        pwm_set_enabled(slice_num, true);
+    }
+
+    gpio_set_irq_enabled_with_callback(ROT_A, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(ROT_SW, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(BUTTON1_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(BUTTON2_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(BUTTON3_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+
+    queue_init(&irqEvents, sizeof(int), BUFFER_SIZE);
+
+    while (true)
+    {
+        while (queue_try_remove(&events, &value))
+        {
+            printf("Got event: %d\r\n", value);
+        }
+    }
+    
+}
