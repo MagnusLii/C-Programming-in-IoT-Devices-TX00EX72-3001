c0b969e25c9f8f337f97051b59cea63a94f64dde
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-11-26 18:46:13 +0200

    Update app.py

--- C/International-Sensor-Development-Project-TX00CR41-3007/app.py
+++ C/International-Sensor-Development-Project-TX00CR41-3007/app.py
@@ -1,51 +1,44 @@
-import eventlet
-import json
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 from flask_mqtt import Mqtt
-from flask_socketio import SocketIO
-from flask_bootstrap import Bootstrap
-
-eventlet.monkey_patch()
 
 app = Flask(__name__)
-app.config['SECRET'] = 'my secret key'
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['MQTT_BROKER_URL'] = '192.168.1.113'  # Update with your MQTT broker IP
-app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_USERNAME'] = ''
-app.config['MQTT_PASSWORD'] = ''
-app.config['MQTT_KEEPALIVE'] = 5
-app.config['MQTT_TLS_ENABLED'] = False
-app.config['MQTT_CLEAN_SESSION'] = True
-
+app.config["MQTT_BROKER_URL"] = "192.168.1.118"  # Replace with broker IP
+#app.config['MQTT_USERNAME'] = ""  # Will be set once we start using authentication
+#app.config['MQTT_PASSWORD'] = ""
+app.config["MQTT_BROKER_PORT"] = 1883
+app.config["MQTT_KEEPALIVE"] = 60
+app.config["MQTT_TLS_ENABLED"] = False
+topic = "/test/topic"  # Replace with topic you want to subscribe to
 mqtt = Mqtt(app)
-socketio = SocketIO(app)
-bootstrap = Bootstrap(app)
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+   if rc == 0:
+       print('Connected successfully')
+       mqtt.subscribe(topic) # subscribe topic
+   else:
+       print('Bad connection. Code:', rc)
 
 
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
-    mqtt.subscribe('/endToSrv')
-    mqtt.publish('/srvToEnd', 'Hello flask server')
+    mqtt.subscribe(topic)  # Subscribe to the topic
+    print("Connected to MQTT broker")
 
 
 @mqtt.on_message()
-def handle_mqtt_message(client, userdata, message):
-    if message.topic == '/endToSrv' and message.payload.decode() == 'Hello end device':
-        mqtt.publish('/srvToEnd', 'Message received')
-    elif message.topic == '/srvToEnd' and message.payload.decode() == 'Message received':
-        mqtt.publish('/endToSrv', 'Hello end device')
+def handle_message(client, userdata, message):
+    received_message = message.payload.decode("utf-8")
+    print("Received message:", received_message)
+    # Process the received message here
+    if received_message == "Hello":
+        mqtt.publish(topic, "Hi there!")  # Publish a response message
 
 
-@mqtt.on_log()
-def handle_logging(client, userdata, level, buf):
-    print(level, buf)
+@app.route("/")
+def index():
+    return "Flask MQTT Server is running!"
 
 
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
