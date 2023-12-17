c246f6497def340ce721d5c21af9032a9d48997a
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-12-17 04:43:22 +0200

    Update db.py

--- International-Sensor-Development-Project-TX00CR41-3007/imports/db.py
+++ International-Sensor-Development-Project-TX00CR41-3007/imports/db.py
@@ -9,12 +9,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-
+# Database models
 class ESPDevice(db.Model):
     DeviceID = db.Column(db.String(17), primary_key=True)
     RegistrationTime = db.Column(db.TIMESTAMP, default=datetime.utcnow)
     LastActiveTime = db.Column(db.TIMESTAMP)
-
 
 class User(db.Model):
     UserID = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -22,14 +21,12 @@ class User(db.Model):
     DeviceID = db.Column(db.String(17))
     RegistrationDate = db.Column(db.TIMESTAMP, default=datetime.utcnow)
 
-
 class Topic(db.Model):
     TopicID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Title = db.Column(db.Text, nullable=False)
     Description = db.Column(db.Text)
     StartTime = db.Column(db.TIMESTAMP)
     EndTime = db.Column(db.TIMESTAMP)
-
 
 class Vote(db.Model):
     VoteID = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -39,8 +36,12 @@ class Vote(db.Model):
     VoteTime = db.Column(db.TIMESTAMP, default=datetime.utcnow)
 
 
+# Database functions
 def create_tables():
     db.create_all()
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
