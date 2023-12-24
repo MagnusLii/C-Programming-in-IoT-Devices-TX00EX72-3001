c52a7cc0e25b6c369f9fd805874932d9e604fb49
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-12-24 13:30:42 +0200

    Update esp.py

--- International-Sensor-Development-Project-TX00CR41-3007/isdProjectImports/esp.py
+++ International-Sensor-Development-Project-TX00CR41-3007/isdProjectImports/esp.py
@@ -7,14 +7,14 @@ class Esp:
     numRegistredESPs = 0
     regiseredESPs = []
 
-    def __init__(self, mac_address, registeredUser='NULL',):
-        self.macAddress = mac_address
+    def __init__(self, macAddress, registeredUser='NULL',):
+        self.macAddress = macAddress
         self.registeredUser = registeredUser
 
         self.uniqueID = str(uuid.uuid4())  # random unique ID for each ESP based on RFC 4122 standards
         self.voteStatus = 'pass'  # default value is pass/abstain.
 
-        self.registration_confirmation_topic = mqttImports.registrationResponeTopic + self.mac_address
+        self.registration_confirmation_topic = mqttImports.registrationResponeTopic + self.macAddress
 
 
         Esp.numRegistredESPs += 1
