b7db4248b3e6e2b131c07e7e19ede4bf7296aa7c
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-12-26 16:11:18 +0200

    Update dbFunctions.py

--- International-Sensor-Development-Project-TX00CR41-3007/isdProjectImports/dbFunctions.py
+++ International-Sensor-Development-Project-TX00CR41-3007/isdProjectImports/dbFunctions.py
@@ -42,12 +42,7 @@ class Votes(db.Model):
 
 def get_registered_esps():
     registered_esps = (
-        RegisteredESPs.query
-        .filter(RegisteredESPs.Registered == True)
-        .options(
-            db.relationship(RegisteredESPs.users, back_populates="esp"),
-        )
-        .all()
+        db.session.query(RegisteredESPs, Users).join(Users, RegisteredESPs.DeviceIndex == Users.DeviceIndex).filter(RegisteredESPs.Assigned == True).all()
     )
 
     esp_data = []
@@ -62,12 +57,12 @@ def get_registered_esps():
             "Users": []
         }
 
-        for user in esp.users:
+        for user in registered_esps:
             user_info = {
                 "UserID": user.UserID,
                 "Username": user.Username,
                 "RegistrationDate": str(user.RegistrationDate),
-                "DeviceIndex": user.DeviceIndex,
+                "DeviceIndex": user.DeviceIndex
             }
             esp_info["Users"].append(user_info)
 
